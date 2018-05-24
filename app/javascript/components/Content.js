6a69ba1d7eea43756404a6920dda8d99c35854dc
KimISeob <seobew@gmail.com>
2018-05-24 16:25:13 +0900

    render contentlist, contents using components, and 'rails webpacker:install:react' for fixing error 'Couldn't find preset 'react'

--- app/javascript/components/Content.js
+++ app/javascript/components/Content.js
@@ -1,10 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
+
 class Content extends React.Component {
   render () {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <div className="Content">
+        <h6>title : {this.props.title} || description : {this.props.description} || views : {this.props.views} || downloads : {this.props.downloads}</h6>
+      </div>
     );
   }
 }
--- app/javascript/components/Contentlist.js
+++ app/javascript/components/Contentlist.js
@@ -1,9 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Content from "./Content.js"
+
 class Contentlist extends React.Component {
   render () {
+    var contentNodes = this.props.data.map(function (content, index){
+      return(
+        <Content title={content.title} description={content.description}
+        views={content.views} downloads={content.downloads} key={content.id}/>
+      );
+    });
     return (
       <React.Fragment>
+        <h1>ContentList</h1>
+        {contentNodes}
       </React.Fragment>
     );
   }
